72208fa3d88b7b88f040acdb4eec3eaa4d88a6c9
Firsoova08 <foxalishca@mail.ru>
2025-11-07 19:01:07 +0300

    git

--- LAB8FirsovaAlekseeva/Program.cs
+++ LAB8FirsovaAlekseeva/Program.cs
@@ -3,8 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-
+        { 
             //Person person = new Person();
             //person.Name = "Ivan";
             //string personName = person.Name;
